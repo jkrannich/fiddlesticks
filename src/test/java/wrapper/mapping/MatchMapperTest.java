31b96d015871b8fdd0c54a5954e18fc96cf1d66a
Julian Krannich <julian.krannich1@gmail.com>
2025-11-05 21:42:20 +0100

    exception thrown mapper test

--- src/test/java/wrapper/mapping/MatchMapperTest.java
+++ src/test/java/wrapper/mapping/MatchMapperTest.java
@@ -1,6 +1,7 @@
 package wrapper.mapping;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import core.dto.match.Info;
 import core.dto.match.MatchDto;
 import core.dto.match.Metadata;
@@ -57,5 +58,46 @@ class MatchMapperTest {
         assertThat(summary.deaths()).isEqualTo(10);
         assertThat(summary.assists()).isEqualTo(5);
         assertThat(summary.win()).isTrue();
+    }
+
+    @Test
+    void toSummaryForPuuid_shouldThrowWhenPuuidNotFound() {
+        String puuid = "test-puuid";
+        String wrongPuuid = "wrong-puuid";
+        Participant participant = new Participant(
+                wrongPuuid,
+                "Faker",
+                "Ahri",
+                10,
+                10,
+                2,
+                1,
+                false,
+                100,
+                100,
+                0,
+                0,
+                10000,
+                500,
+                5000,
+                null
+        );
+
+        Metadata metadata = new Metadata("2", "EUW1_123456", List.of(puuid));
+        Info info = new Info(
+                "CLASSIC",
+                "RANKED_GAME",
+                "14.1.1",
+                1800L,
+                1234567L,
+                List.of(participant),
+                List.of(),
+                "420"
+        );
+        MatchDto matchDto = new MatchDto(metadata, info);
+
+        assertThatThrownBy( () -> MatchMapper.toSummaryForPuuid(matchDto, puuid))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("No participant found for puuid: " + puuid);
     }
 }
