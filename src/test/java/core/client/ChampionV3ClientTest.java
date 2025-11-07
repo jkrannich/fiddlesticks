7abe65439c80b76a1af846f5947fb7c9d59c5356
Julian Krannich <julian.krannich1@gmail.com>
2025-11-07 23:58:41 +0100

    unit test for champion rotation client

---
+++ src/test/java/core/client/ChampionV3ClientTest.java
@@ -0,0 +1,59 @@
+package core.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import core.config.Regions;
+import core.dto.championRotation.ChampionInfoDto;
+import core.http.ApiResponse;
+import core.http.RiotHttp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+@ExtendWith(MockitoExtension.class)
+class ChampionV3ClientTest {
+    @Mock
+    private RiotHttp riotHttp;
+
+    private ChampionV3Client championV3Client;
+
+    @BeforeEach
+    void setUp() {
+        championV3Client = new ChampionV3Client(riotHttp);
+    }
+
+    @Test
+    void getChampionRotation_shouldReturnChampionInfoDto() {
+        Regions.PlatformRegion platform = Regions.PlatformRegion.EUW1;
+
+        ChampionInfoDto expected = new ChampionInfoDto(
+                10,
+                List.of(1,2,3),
+                List.of(244, 222, 333)
+        );
+
+        ApiResponse<ChampionInfoDto> response = new ApiResponse<>(200, Map.of(), expected);
+        when(riotHttp.get(any(URI.class), eq(ChampionInfoDto.class))).thenReturn(response);
+
+        ChampionInfoDto result = championV3Client.getChampionRotation(platform);
+
+        assertThat(result).isNotNull();
+        assertThat(result.maxNewPlayerLevel()).isEqualTo(expected.maxNewPlayerLevel());
+        assertThat(result.freeChampionIdsForNewPlayers()).isEqualTo(expected.freeChampionIdsForNewPlayers());
+        assertThat(result.freeChampionIds()).isEqualTo(expected.freeChampionIds());
+
+        verify(riotHttp).get(
+                eq(URI.create("https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations")),
+                eq(ChampionInfoDto.class)
+        );
+    }
+}
