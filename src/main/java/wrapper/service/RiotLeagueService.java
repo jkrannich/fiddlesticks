ad839647116277ab5af38814449cc585ef258e49
Julian K <julian.krannich1@gmail.com>
2025-11-18 20:37:47 +0100

    wrapper class and method to fetch match history of given player

--- src/main/java/wrapper/service/RiotLeagueService.java
+++ src/main/java/wrapper/service/RiotLeagueService.java
@@ -1,4 +1,37 @@
 package wrapper.service;
 
-public class RiotLeagueService {
+import core.RiotApi;
+import core.config.Regions;
+import wrapper.domain.MatchSummary;
+import wrapper.mapping.MatchMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class RiotLeagueService {
+
+    private final RiotApi riotApi;
+
+    public RiotLeagueService(final RiotApi riotApi) {
+        this.riotApi = riotApi;
+    }
+
+    public String getPuuid(final Regions.RegionalRoute route, final String name, final String tag) {
+        return riotApi.account().byRiotId(route, name, tag).puuid();
+    }
+
+    public List<MatchSummary> getMatchHistory(final Regions.RegionalRoute route, final String name, final String tag, int limit) {
+        final String puuid = getPuuid(route, name, tag);
+
+        final String[] matchIds = riotApi.match().getListOfMatchIdsByPuuid(route, puuid, 0, limit);
+
+        final List<MatchSummary> summaries = new ArrayList<>();
+
+        for (String matchId : matchIds) {
+            var dto = riotApi.match().getMatchByMatchId(route, matchId);
+            var summary = MatchMapper.toSummaryForPuuid(dto, puuid);
+            summaries.add(summary);
+        }
+        return summaries;
+    }
 }
